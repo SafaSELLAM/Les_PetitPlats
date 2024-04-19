c1fa97f6eed131dce66e33f34c80933e6171a693
SafaSELLAM <safa.sellam@outlook.fr>
2024-04-19 05:40:45 +0200

    New - get main code ready to implement 2 filter functions

--- js/filters.js
+++ js/filters.js
@@ -1,11 +1,12 @@
 import { getRecipes } from "./fetch.js";
-import { FilterManager } from "./main.js";
+import { FiltersManager } from "./main.js";
+import { displayRecipes } from "./cardTemplate.js";
 /**
  * Ajouter les écouteurs d'événements pour les éléments de saisie de chaque filtre
  * @param {[string]} inputId : Id des barre de recherches (serarchbar_)
- * @param {[string]} dropdownId : Id des dropdown_ingrédients, dropdown_ustensils, dropdown_appliances
+ * @param {[string]} dropdownId : Id des dropdown_ingrédients, dropdown-ustensils, dropdown-appliances
  */
-function addFilterListener(inputId, dropdownId) {
+function addFiltersListener(inputId, dropdownId) {
     document.getElementById(inputId).addEventListener("keyup", () => {
         filterFunction(inputId, dropdownId);
     });
@@ -13,7 +14,7 @@ function addFilterListener(inputId, dropdownId) {
 /**
  * filtrer les éléments en fonction de la saisie utilisateur
  * @param {[string]} inputId : Id des barre de recherches (serarchbar_)
- * @param {[string]} dropdownId : Id des dropdown_ingrédients, dropdown_ustensils, dropdown_appliances
+ * @param {[string]} dropdownId : Id des dropdown_ingrédients, dropdown-ustensils, dropdown-appliances
  */
 function filterFunction(inputId, dropdownId) {
     const input = document.getElementById(inputId);
@@ -30,16 +31,11 @@ function filterFunction(inputId, dropdownId) {
  *Récupération des éléments de filtres (ingrédients, ustensiles, appareils)
  * @returns {{ allIngredients: [string], allAppliances: [string], allUstensils: [string] }} 
  */
-export async function getAllValues() {
+export function getAllValues(recipe, allIngredients, allUstensils, allAppliances) {
 
-    const recipesInfo = await getRecipes()
-    let allIngredients = [], allAppliances = [], allUstensils = [];
-
-    recipesInfo.forEach(recipe => {
-        allIngredients = [...new Set(textOptionsNormalizer(recipe.ingredients, allIngredients, true))];
-        allUstensils = [...new Set(textOptionsNormalizer(recipe.ustensils, allUstensils))];
-        allAppliances = [...new Set(textOptionsNormalizer([recipe.appliance], allAppliances))];
-    });
+    allIngredients = [...new Set(textOptionsNormalizer(recipe.ingredients, allIngredients, true))];
+    allUstensils = [...new Set(textOptionsNormalizer(recipe.ustensils, allUstensils))];
+    allAppliances = [...new Set(textOptionsNormalizer([recipe.appliance], allAppliances))];
 
     return { allIngredients, allAppliances, allUstensils };
 }
@@ -55,19 +51,79 @@ function textOptionsNormalizer(type, allTypes, hasIngredient = false) {
     type.forEach(element => {
 
         let normalizedType = hasIngredient ? element.ingredient.toLowerCase() : element.toLowerCase();
+        normalizedType = singularize(normalizedType);
         normalizedType = normalizedType.charAt(0).toUpperCase() + normalizedType.slice(1);
         allTypes.push(normalizedType);
     });
     return allTypes
 }
+/**
+ * Convertir un mot au singulier
+ * @param {string} word : mot à convertir
+ * @returns {string} : mot au singulier
+ */
+function singularize(word) {
+
+    if (word.endsWith("s") && word !== "ananas" && word !== "maïs") {
+        return word.slice(0, -1);
+    }
+    return word;
+}
 
 // Instance de la classe FilterManager
-const filterManager = new FilterManager();
+const filterManager = new FiltersManager();
 
 // Initialisation des filtres
-filterManager.initializeFilters();
+const allItems = await filterManager.initializeFilters();
+filterManager.initializeInputEvents();
 
 // Ajouter les écouteurs d'événements pour les filtres
-addFilterListener("searchBar_ingredient", "dropdown_ingredients");
-addFilterListener("searchBar_appareils", "dropdown_appliances");
-addFilterListener("searchBar_ustensiles", "dropdown_ustensils");
+addFiltersListener("searchbar-ingredient", "dropdown-ingredients");
+addFiltersListener("searchbar-appliances", "dropdown-appliances");
+addFiltersListener("searchbar-ustensils", "dropdown-ustensils");
+
+
+
+// Gestion barre de filtre principale
+async function globalSearch() {
+    const userResearch = this.value.toLowerCase().trim();
+    const recipes = await getRecipes();
+    const totalRecipes = recipes.length;
+    if (userResearch.length >= 3) {
+        isAllRecipesLoaded = false
+        document.getElementById('recipe-cards').innerHTML = "";
+        const finalRecipes = filterRecipes(userResearch, recipes);
+        updateDisplayRecipes(finalRecipes)
+    } else {
+        if (!isAllRecipesLoaded) {
+            document.getElementById('recipe-cards').innerHTML = "";
+            recipes.forEach(recipe => {
+                displayRecipes(recipe)
+            });
+            isAllRecipesLoaded = true
+            filterManager.addAllItemsToFiltersList(allItems, filterManager);
+        }
+        document.getElementById('number-recipes').innerText = totalRecipes + " recettes";
+    }
+
+}
+const mainSearchInput = document.getElementById('main-searchbar');
+let isAllRecipesLoaded = false
+mainSearchInput.addEventListener('input', globalSearch)
+
+/**
+ * Gère l'affichage des recettes après le filtres des recettes
+ * @param {array} recipes : recettes à afficher
+ */
+function updateDisplayRecipes(recipes) {
+    let allIngredients = [], allAppliances = [], allUstensils = [];
+    for (const recipe of recipes) {
+        displayRecipes(recipe);
+        const allItems = getAllValues(recipe, allIngredients, allUstensils, allAppliances)
+        filterManager.addAllItemsToFiltersList(allItems, filterManager);
+
+    }
+    document.getElementById('number-recipes').innerText = recipes.length + " recettes";
+}
+
+
